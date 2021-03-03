e54ab4e82d2ea40626563d0e1dacff976d8c899c
ulugbek-ubuntu-20 <utn1002@gmail.com>
2021-03-03 18:59:39 +0500

    as

---
+++ zoom24022021/while_demo.py
@@ -0,0 +1,24 @@
+# 1. chiziqli
+# 2. tarmoqlanuvchi
+# 3. takrorlanuvchi
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+# for number in numbers:
+#     print(number)
+
+# range(start=0,stop=5,step=1)
+# for i in range(5):
+#     print(i)
+
+# for i in range(100):
+#     if i % 5 == 0:
+#         print(i, end=" ")
+
+# for i in range(5, 100, 5): # [5, 10, .... 95, 100]
+#         print(i, end=" ")
+
+text = "python"
+for belgi in text:
+    print(belgi, end=" ")
+
